df5e83fd258362635023378c1b7c542e308f25de
Shivamsingh-Rawat <singhshivam482004@gmail.com>
2024-07-03 07:23:47 +0530

    LoginIn.js

--- src/App.js
+++ src/App.js
@@ -14,20 +14,21 @@ function App() {
   const dispatch = useDispatch();
   const lightTheme = useSelector((state) => state.themeKey);
 
-  return( <div className={"App" + (lightTheme ? "" : "-dark")}>
-    {/* <MainContainer />  */}
-    {/* <LogIn /> */}
-    <Routes>
-      <Route path="/" element={<LogIn />} ></Route>
-      <Route path="app" element={<MainContainer />}>
-        <Route path="welcome" element={<Welcome />}></Route>
-        <Route path="chat/:_id" element={<ChatArea />}></Route>
-        <Route path="users" element={<Users />}></Route>
-        <Route path="groups" element={<Groups />}></Route>
-        <Route path="create-groups" element={<CreateGroups />}></Route>       
-      </Route>
-    </Routes>
-  </div>
+  return (
+    <div className={"App" + (lightTheme ? "" : "-dark")}>
+      {/* <MainContainer />  */}
+      {/* <LogIn /> */}
+      <Routes>
+        <Route path="/" element={<LogIn />}></Route>
+        <Route path="app" element={<MainContainer />}>
+          <Route path="welcome" element={<Welcome />}></Route>
+          <Route path="chat/:_id" element={<ChatArea />}></Route>
+          <Route path="users" element={<Users />}></Route>
+          <Route path="groups" element={<Groups />}></Route>
+          <Route path="create-groups" element={<CreateGroups />}></Route>
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
